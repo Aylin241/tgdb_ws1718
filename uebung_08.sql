4668ccd284056ce589ff29ff0f7945c00c9493c8
Aylin241 <32551441+Aylin241@users.noreply.github.com>
2017-11-27 14:32:22 +0100

    Update uebung_08.sql

--- uebung_08.sql
+++ uebung_08.sql
@@ -7,21 +7,15 @@ v_id account.account_id%TYPE;
 BEGIN
 select max(account_id) +1 INTO v_id 
 from account;
-
 Insert into account
 values(v_id, surname, forename, email, c_date, u_date);
 
 DBMS_OUTPUT.PUT_LINE('Account mit der ID : ' || v_id || 'angelegt');
 
 EXCEPTION
-
 WHEN OTHERS THEN
 RAISE_APPLICATION_ERROR(-20030, 'Unbekannter Fehler' );
-
 END;
 /
 
-
 EXEC benutzer ('Yildirim', 'Aylin', 'aylinblaxk', sysdate, sysdate);
-
-2.) 
