f148a7b982a52bd7e4ad7313f3e1eb3630142d85
Aylin241 <32551441+Aylin241@users.noreply.github.com>
2017-11-27 14:30:13 +0100

    Add files via upload

---
+++ uebung_08.sql
@@ -0,0 +1,32 @@
+1.) /*
+Prezedur, die das anlegen von Benutzern duch übergabe von Paramentern ermöglicht
+*/
+
+Create or replace PROCEDURE benutzer (surname IN VARCHAR2, forename IN VARCHAR2, email IN VARCHAR2, c_date IN DATE, u_date IN DATE) 
+
+AS
+
+v_id account.account_id%TYPE;
+
+BEGIN
+
+select max(account_id) +1 INTO v_id 
+from account;
+
+Insert into account
+values(v_id, surname, forename, email, c_date, u_date);
+
+DBMS_OUTPUT.PUT_LINE('Account mit der ID : ' || v_id || 'angelegt');
+
+EXCEPTION
+
+WHEN OTHERS THEN
+RAISE_APPLICATION_ERROR(-20030, 'Unbekannter Fehler' );
+
+END;
+/
+
+
+EXEC benutzer ('Yildirim', 'Aylin', 'aylinblaxk', sysdate, sysdate);
+
+2.) 
